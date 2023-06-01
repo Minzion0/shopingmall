6d192a3ae7ae2101b5652f482ba1b5ff80392ce7
minzion0 <aert12435@gmail.com>
2023-06-01 19:13:14 +0900

    처음

---
+++ src/test/java/com/green/shopingmall/ShopingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.shopingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
